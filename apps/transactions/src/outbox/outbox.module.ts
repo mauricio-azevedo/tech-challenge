504de8a717ddfd23938d6bec058220f4bbeb3d9a
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2026-08-30 10:39:59 -0300

    feat(transactions): publica os eventos do outbox no kafka com um relay

--- apps/transactions/src/outbox/outbox.module.ts
+++ apps/transactions/src/outbox/outbox.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { OutboxRelay } from './outbox-relay.service.js';
 import { OutboxRepository } from './outbox.repository.js';
 
 @Module({
-  providers: [OutboxRepository],
-  exports: [OutboxRepository],
+  providers: [OutboxRepository, OutboxRelay],
+  exports: [OutboxRepository, OutboxRelay],
 })
 export class OutboxModule {}
